93afa1297c16f371099af65c1018d693cdf636d3
ITALO_2007 <132021516+ITA700@users.noreply.github.com>
2023-05-22 11:09:49 -0300

    Add files via upload

---
+++ Calculo_Fahrenheit_e_Celsius.py
@@ -0,0 +1,6 @@
+c = float(input("Digite a temperatura da sua cidade *Obrigatoriamente em C° "))
+conversao_fahrenheit = 1.8*c+32
+print("A sua temperatura convertida de celsius para fahrenheit é: ", conversao_fahrenheit, " °F")
+f = float(input("Digite a temperatura da sua cidade *Obrigatoriamente em F° "))
+conversao_celsius = (f-32)/1.8
+print("A sua temperatura convertida de celsius para fahrenheit é: ", conversao_celsius, " °C")
